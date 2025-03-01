48294a51c798e93db849c51c4b6752af18971811
Daniel David Perez Coapango <dano.prez.coa@gmail.com>
2025-02-28 22:36:42 -0600

    Commit initial

---
+++ src/main/java/com/unir/backend/operator_microservice/OperatorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.unir.backend.operator_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/backend/operator_microservice/OperatorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.backend.operator_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
